cec3e40e0a4d197c703cf1c9ddeae326399416fa
Van Hour <vannhour15@gmail.com>
2026-06-10 19:20:26 +0700

    Main branch structure

--- Main.cpp
+++ Main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 using namespace std;
 
-    int main(){
-        
+int func1();
+int func2();
+int func3();
 
-
-    };
+int main() {
+    cout << func1() << endl;
+    cout << func2() << endl;
+    cout << func3() << endl;
+    return 0;
+};
