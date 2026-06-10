f91e3505dea868768cb10bc1f90a9ab872835ae4
Van Hour <vannhour15@gmail.com>
2026-06-10 19:23:34 +0700

    testing for first function

--- Main.cpp
+++ Main.cpp
@@ -1,2 +1,10 @@
-/// testing stuff
-/// Testin stuff 2
+#include <iostream>
+using namespace std;
+
+int func1() {
+    int x = 2, y = 3;
+    int z = x + y;
+
+    cout << "The sum of x and y is: " << z << endl; 
+    return 1;
+};
